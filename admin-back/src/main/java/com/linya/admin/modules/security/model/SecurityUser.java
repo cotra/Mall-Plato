751f31cafa9eaf00ba2dbe64399e0c17d0e41a35
cotra <mr03web@foxmail.com>
2019-07-12 18:12:55 +0800

    Spring Security配置

---
+++ admin-back/src/main/java/com/linya/admin/modules/security/model/SecurityUser.java
@@ -0,0 +1,56 @@
+package com.linya.admin.modules.security.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SecurityUser implements UserDetails {
+
+    private String username;
+    private String password;
+    private Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+    public SecurityUser(String username, String password) {
+        this.password = password;
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ admin-back/src/main/java/com/linya/admin/modules/security/service/AppUserDetailsService.java
@@ -0,0 +1,13 @@
+package com.linya.admin.modules.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
