6e336cb918f4e2b1b22aaf5f7049e6aa855a2bba
cotra <mr03web@foxmail.com>
2019-07-26 14:19:51 +0800

    邮件

---
+++ admin-back/src/main/java/com/linya/admin/center/mail/MailCenter.java
@@ -0,0 +1,31 @@
+package com.linya.admin.center.mall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MallCenter {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("mr03web@qq.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+
+        try {
+            mailSender.send(message);
+            log.info("简单邮件已经发送。");
+        } catch (Exception e) {
+            log.error("发送简单邮件时发生异常！", e);
+        }
+
+    }
+}
